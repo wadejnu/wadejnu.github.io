c09f031d0ac9e7d6b3e8bf53d58a6819a5a13f54
wade <3216353998@qq.com>
2025-02-09 21:53:44 +0800

    add lesson note in js const

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,6 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards']
-
+const section_names = ['home', 'publications', 'awards', 'lesson-note']
 
 window.addEventListener('DOMContentLoaded', event => {
 
